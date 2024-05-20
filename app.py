7e44b1a73e0b42537c478ac1d1a2e413792952b0
pccusir <wfx2@ulive.pccu.edu.tw>
2024-05-20 14:24:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    line_bot_api.reply_message(event.reply_token, TextSendMessage('QA Error'))
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(msg))
          
 
 @handler.add(PostbackEvent)
